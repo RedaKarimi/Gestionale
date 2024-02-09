2c4c0746035130847d8049ebce9a6e13438139d8
RedaKarimi <Redakarimi76@gmail.com>
2024-02-09 15:44:02 +0100

    new file:   Client/frontend.md 	modified:   Server/obj/Debug/net8.0/Server.AssemblyInfo.cs 	modified:   Server/obj/Debug/net8.0/Server.AssemblyInfoInputs.cache

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9cf1c30ae71dfa463c341e2e328ae5e753b020")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
